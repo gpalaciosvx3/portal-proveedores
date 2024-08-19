027d348c153335e94783cddc69fa039f89da1352
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2024-08-18 20:50:46 -0500

    chore: Iniciando backend (express) and frontend (react) projects

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express();
+const port = process.env.PORT || 5000;
+dotenv.config();
+
+/* MIDEELWARE */
+// Middelware to parse cookie's and json
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+// My own routes middelware
+
+// end of middelware
+
+app.listen(port, () =>{
+    connect();
+    console.log(`Connected to backend at port ${port}`);
+});
